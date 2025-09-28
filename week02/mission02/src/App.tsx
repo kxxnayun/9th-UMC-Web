49924c93b8ac513a443e7c007cf8f9df55c923d8
kxxxnayun <nayun2179@gmail.com>
2025-09-28 21:25:30 +0900

    Rename folder

--- week02/mission02/src/App.tsx
+++ week02/mission02/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 import ContextPage from "./context/ContextPage";
-import { ThemeProvider } from './context/ThemeProvider';
+import { ThemeProvider } from "./context/ThemeProvider";
 
 export function App() {
   return (
     <ThemeProvider>
       <ContextPage></ContextPage>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
